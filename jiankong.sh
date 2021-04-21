e0b9d211f75c7b811238aa8ac57c16114315b201
satoshin546 <58742849+satoshin546@users.noreply.github.com>
2021-04-21 12:27:27 +0800

    Update jiankong.sh

--- jiankong.sh
+++ jiankong.sh
@@ -1,7 +1,10 @@
+
 #!/bin/bash
 ss=`dstat -n 1 10 | grep -c "B"`
-if [ $ss>5 ];then
-echo "有$ss次B字符，准备重启..."
+if [ $ss -gt 5 ];then
+echo "有$ss次B字符,准备重启..."
 docker restart ssrmu
 echo "重启成功！"
+else
+echo "有$ss次B字符，无需操作！"
 fi
